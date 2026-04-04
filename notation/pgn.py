6accffd01f9a9e73708452e610ceebf6456bc853
ildanin <ilya.a.danilenko@gmail.com>
2026-04-04 18:18:05 +0300

    refactor: remove commented lines

--- notation/pgn.py
+++ notation/pgn.py
@@ -1,4 +1,6 @@
 from notation.fen import ForsythEdwardsNotation
+from notation.square import BoardMove
+from positionClass import Position
 
 #pgn notation
 #1. e4 e6 2. d4 b6 3. a3 Bb7 4. Nc3 Nh6 5. Bxh6 gxh6 6. Be2 Qg5 7. Bg4 h5 8. Nf3 Qg6 9. Nh4 Qg5 10. Bxh5 Qxh4 11. Qf3 Kd8 12. Qxf7 Nc6 13. Qe8# 1-0
@@ -6,12 +8,10 @@ class PortableGameNotation:
     def __init__(self, PGN: str, init_position: ForsythEdwardsNotation = ForsythEdwardsNotation()) -> None:
         self.string = PGN
         self.init_position = init_position
+        self.position = Position(init_position)
     
     def __str__(self) -> str:
         return self.string
-
-    def append(self, move) -> None:
-        pass
     
     def get_results(self) -> str:
         return self.string.split()[-1]
@@ -37,21 +37,3 @@ class PortableGameNotation:
                     move = 'p' + move
             formatted_move_list.append(move)
         return formatted_move_list
-
-
-    '''def get_position_array(self, move: int) -> list[str]:
-        pass
-
-    def get_position_array_list(self) -> list[list[str]]:
-        position_array = self.init_position.get_position_array()'''
-
-
-    '''def as_FEN_list(self) -> list[ForsythEdwardsNotation]: #todo
-        pass
-
-    def as_FEN(self, move: int) -> ForsythEdwardsNotation: #todo
-        pass'''
-
-'''pgn = PortableGameNotation("1. e4 e6 2. d4 b6 3. a3 Bb7 4. Nc3 Nh6 5. Bxh6 gxh6 6. Be2 Qg5 7. Bg4 h5 8. Nf3 Qg6 9. Nh4 Qg5 10. Bxh5 Qxh4 11. Qf3 Kd8 12. Qxf7 Nc6 13. Qe8# 1-0")
-print(pgn.get_move_list())
-print(pgn.get_formatted_move_list())'''
