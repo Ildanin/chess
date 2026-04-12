7e5b863eefc039d97240a89e64a383e0e05cedd8
ildanin <ilya.a.danilenko@gmail.com>
2026-04-12 20:59:24 +0300

    refactor: move chessAI to chessAI directory

---
+++ chessAI/__init__.py
@@ -0,0 +1,8 @@
+from network import Network
+import numpy as np
+from config import INFO, ACTIVATOR, NORMALIZER, FACTOR_RANGE, BIAS_RANGE, ALPHA, CYCLES
+from data import get_games
+from positionClass import Position
+
+class ChessAI(Network):
+    pass
