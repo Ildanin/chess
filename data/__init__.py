2764aaf9aeb4633e1641783c9744c152cef83b64
ildanin <ilya.a.danilenko@gmail.com>
2026-04-12 20:58:11 +0300

    refactor: move generate_games_file function to transform.py

--- data/__init__.py
+++ data/__init__.py
@@ -1,57 +1,8 @@
-import os
-datasets_path = os.path.dirname(__file__)
-
-def generate_games_file(input_filename: str, output_filename: str | None = None) -> str:
-    "Returns the file with games in descending average ELO order"
-    if output_filename == None:
-        output_filename = input_filename.partition('.')[0] + '.txt'
-    data: list[tuple] = []
-    with open(os.path.join(datasets_path, input_filename)) as inp_file:
-        for line in inp_file:
-            if line == '\n':
-                pass
-            elif line.split()[0][1:] == "WhiteElo":
-                if line.split('"')[1] != '?':
-                    white_elo = int(line.split('"')[1])
-                else:
-                    white_elo = 1000
-            elif line.split()[0][1:] == "BlackElo":
-                if line.split('"')[1] != '?':
-                    black_elo = int(line.split('"')[1])
-                else:
-                    black_elo = 1000
-                avg_elo = (black_elo + white_elo) // 2
-            elif line.split()[0] == '1.':
-                game = remove_evaluations(line)
-                game = remove_assessments(game)
-                data.append((avg_elo, game))
-    with open(os.path.join(datasets_path, output_filename), 'w') as out_file:
-        for avg_elo, game in sorted(data, reverse=True):
-            out_file.write(game)
-    return output_filename
-
-def remove_evaluations(pgn_string: str) -> str:
-    while pgn_string.count('{') > 0:
-        start = pgn_string.index('{')
-        stop = pgn_string.index('}')
-        pgn_string = pgn_string[:start-1] + pgn_string[stop+1:]
-    while pgn_string.count('...') > 0:
-        start = pgn_string.index('...')
-        pgn_string = pgn_string[:start-2] + pgn_string[start+3:]
-    return pgn_string
-
-def remove_assessments(pgn_string: str) -> str:
-    pgn_string = pgn_string.replace('?', '')
-    pgn_string = pgn_string.replace('!', '')
-    return pgn_string
-
-if __name__ == '__main__':
-    generate_games_file("lichess_db_standard_rated_2013-01.pgn", "games.txt")
-    exit()
-
 from notation.pgn import PortableGameNotation
 from positionClass import Position
 import numpy as np
+import os
+datasets_path = os.path.dirname(__file__)
 
 def position_encode(position: Position) -> str:
     encoded = ''
@@ -88,7 +39,6 @@ def position_encode(position: Position) -> str:
                 case 'q': encoded += '0110000000'
                 case 'k': encoded += '1000000000'
     return encoded
-
 
 def get_data(filename: str, start: int, stop: int) -> tuple[list[np.ndarray], list[np.ndarray]]:
     file = open(os.path.join(datasets_path, filename))
---
+++ data/transform.py
@@ -0,0 +1,49 @@
+import os
+datasets_path = os.path.dirname(__file__)
+
+def generate_games_file(input_filename: str, output_filename: str | None = None) -> str:
+    "Generates the file with games in descending average ELO order"
+    if output_filename == None:
+        output_filename = input_filename.partition('.')[0] + '.txt'
+    data: list[tuple] = []
+    with open(os.path.join(datasets_path, input_filename)) as inp_file:
+        for line in inp_file:
+            if line == '\n':
+                pass
+            elif line.split()[0][1:] == "WhiteElo":
+                if line.split('"')[1] != '?':
+                    white_elo = int(line.split('"')[1])
+                else:
+                    white_elo = 1000
+            elif line.split()[0][1:] == "BlackElo":
+                if line.split('"')[1] != '?':
+                    black_elo = int(line.split('"')[1])
+                else:
+                    black_elo = 1000
+                avg_elo = (black_elo + white_elo) // 2
+            elif line.split()[0] == '1.':
+                game = remove_evaluations(line)
+                game = remove_assessments(game)
+                data.append((avg_elo, game))
+    with open(os.path.join(datasets_path, output_filename), 'w') as out_file:
+        for avg_elo, game in sorted(data, reverse=True):
+            out_file.write(game)
+    return output_filename
+
+def remove_evaluations(pgn_string: str) -> str:
+    while pgn_string.count('{') > 0:
+        start = pgn_string.index('{')
+        stop = pgn_string.index('}')
+        pgn_string = pgn_string[:start-1] + pgn_string[stop+1:]
+    while pgn_string.count('...') > 0:
+        start = pgn_string.index('...')
+        pgn_string = pgn_string[:start-2] + pgn_string[start+3:]
+    return pgn_string
+
+def remove_assessments(pgn_string: str) -> str:
+    pgn_string = pgn_string.replace('?', '')
+    pgn_string = pgn_string.replace('!', '')
+    return pgn_string
+
+if __name__ == '__main__':
+    generate_games_file("lichess_db_standard_rated_2013-01.pgn", "games.txt")
