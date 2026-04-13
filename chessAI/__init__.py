0261f09193e70c7405b6814e2ec3f55000357d95
ildanin <ilya.a.danilenko@gmail.com>
2026-04-13 21:58:40 +0300

    feat: add move function

--- chessAI/__init__.py
+++ chessAI/__init__.py
@@ -7,3 +7,6 @@ class ChessAI(Network):
     def get_move(self, position: Position) -> BoardMove:
         move = super().process(position_encode(position))
         return move_decode(''.join(str(round(bit)) for bit in move))
+    
+    def move(self, position: Position, skip_check: bool = False) -> None:
+        position.move(self.get_move(position), skip_check=skip_check)
