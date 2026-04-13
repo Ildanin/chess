314d473ff0eb3ce852dfbc9afee202376138153f
ildanin <ilya.a.danilenko@gmail.com>
2026-04-13 19:09:49 +0300

    feat: add elo threshold

--- chessAI/data/transform.py
+++ chessAI/data/transform.py
@@ -1,7 +1,7 @@
 import os
 datasets_path = os.path.dirname(__file__)
 
-def generate_games_file(input_filename: str, output_filename: str | None = None) -> str:
+def generate_games_file(input_filename: str, output_filename: str | None = None, elo_threshold: int | None = None) -> str:
     "Generates the file with games in descending average ELO order"
     if output_filename == None:
         output_filename = input_filename.partition('.')[0] + '.txt'
@@ -21,6 +21,8 @@ def generate_games_file(input_filename: str, output_filename: str | None = None)
                 else:
                     black_elo = 1000
                 avg_elo = (black_elo + white_elo) // 2
+                if elo_threshold != None and avg_elo < elo_threshold:
+                    continue
             elif line.split()[0] == '1.':
                 game = remove_evaluations(line)
                 game = remove_assessments(game)
@@ -46,4 +48,4 @@ def remove_assessments(pgn_string: str) -> str:
     return pgn_string
 
 if __name__ == '__main__':
-    generate_games_file("lichess_db_standard_rated_2013-01.pgn", "games.txt")
+    generate_games_file("lichess_db_standard_rated_2013-01.pgn", "data.txt")
